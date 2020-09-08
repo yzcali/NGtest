577d80e8d642519aa48be3e92efc8c48c8661c02
yzcali <yaziciali694@gmail.com>
2020-09-08 22:16:10 +0200

    day15derssonu

--- src/test/java/com/techproed/tests/BeklemeTest.java
+++ src/test/java/com/techproed/tests/BeklemeTest.java
@@ -2,7 +2,11 @@ package com.techproed.tests;
 
 import com.techproed.utilities.Testbase;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class BeklemeTest extends Testbase {
@@ -13,5 +17,19 @@ public class BeklemeTest extends Testbase {
         WebElement removeButonu = driver.findElement(By.xpath("//*[.='Remove']"));
         removeButonu.click();
         WebElement element = driver.findElement(By.xpath("//*[.='Add']"));
+    }
+
+    @Test
+    public void explicitWait(){
+        driver.get("http://the-internet.herokuapp.com/dynamic_controls");
+     // Explicit wait kullanmak icin WebDriverWait class indan nesne uretmemiz lazim.
+       WebDriverWait wait = new WebDriverWait(driver, 30);
+
+        WebElement removeButton = driver.findElement(By.xpath("//*[.='Remove']"));
+        removeButton.click();
+
+        WebElement element=wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
+        System.out.println(element.getText());
+
     }
 }
---
+++ src/test/java/com/techproed/tests/OrnekHotelOlusturma.java
@@ -0,0 +1,63 @@
+package com.techproed.tests;
+
+import com.techproed.utilities.Testbase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class OrnekHotelOlusturma extends Testbase {
+    @BeforeMethod
+    public void entryTest(){
+        driver.get("http://www.fhctrip-qa.com/admin/HotelAdmin/Create");
+        WebElement uN= driver.findElement(By.id("UserName"));
+        uN.sendKeys("manager2");
+        WebElement pW= driver.findElement(By.id("Password"));
+        pW.sendKeys("Man1ager2!"+ Keys.ENTER);
+
+    }
+    @Test
+    public void HotelCreateTest() throws InterruptedException {
+        WebElement code= driver.findElement(By.id("Code"));
+        code.sendKeys("158962");
+        WebElement name= driver.findElement(By.id("Name"));
+        name.sendKeys("jack");
+        Thread.sleep(3000);
+
+        WebElement add= driver.findElement(By.id("Address"));
+        add.sendKeys("15Rue");
+        Thread.sleep(3000);
+
+        WebElement phone= driver.findElement(By.id("Phone"));
+        phone.sendKeys("0666060605");
+
+        Thread.sleep(3000);
+        WebElement email= driver.findElement(By.id("Email"));
+        email.sendKeys("jack@mail.com");
+
+
+        WebElement hotelB= driver.findElement(By.id("IDGroup"));
+        Select select=new Select(hotelB);
+        select.selectByIndex(2);
+
+        driver.findElement(By.id("btnSubmit")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 30) ;
+        WebElement message = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='bootbox-body']")));
+        System.out.println(message.getText());
+
+
+
+    }
+
+    @AfterClass
+    public void tearDown(){
+        //driver.quit();
+    }
+
+}
